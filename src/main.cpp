5cfd766dc7be1c795aa94244978d1d5da471c643
tural.bozkurt <tural.bozkurt999@gmail.com>
2026-09-07 02:31:24 +0400

    yeni

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,8 @@ struct GPSData {
 static GPSData gps;
 
 // ======================== UMUMI SABITLER ========================
-#define DEVICE_HEADER F("CC")
-#define DEVICE_NAME   "DRONE (CC)"
+#define DEVICE_HEADER F("BB")
+#define DEVICE_NAME   "PAYLOAD (BB)"
 #define LED_PIN         13
 #define RF_SERIAL       Serial2
 #define RF_BAUD         115200
